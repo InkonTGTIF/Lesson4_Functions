75e0c17153795410636c2464c5da888c9bd5ebfa
Karpov Kirill <maccdig@gmail.com>
2023-12-03 21:44:14 +0300

    Add Task 1/2/3 and README

---
+++ Task_2/Program.cs
@@ -0,0 +1,25 @@
+﻿int [] CreateArray(int minNum, int maxNum, int size){
+
+    int [] array = new int [size];
+    for(int i = 0; i<size; i++){
+        array[i] = new Random().Next(minNum, maxNum+1);
+    }
+    return array;
+}
+
+void ShowArray(int [] array){
+    int number = 0;
+    for(int i = 0; i<array.Length;i++){
+        if (array[i]%2==0)
+        number++;
+        Console.Write(array[i] + " ");
+        }
+    Console.WriteLine($"=> " + number);
+}
+
+int minNum = 111;
+int maxNum = 999;
+int size = 10;
+
+int [] array = CreateArray(minNum, maxNum, size);
+ShowArray(array);
---
+++ Task_3/Program.cs
@@ -0,0 +1,30 @@
+﻿int [] CreateArray(int minNum, int maxNum, int size){
+
+    int [] array = new int [size];
+    for(int i = 0; i<size; i++){
+        array[i] = new Random().Next(minNum, maxNum+1);
+    }
+    return array;
+}
+
+void ShowArray(int [] array){
+    
+    for(int i=0; i<array.Length; i++){
+        Console.Write(array[i]+" ");
+    }
+    Console.Write($"=> ");
+}
+
+void ShowArrayReverse(int [] array){
+    for(int i=array.Length-1; i>=0; i--){
+        Console.Write(array[i] + " ");
+    }
+}
+
+int minNum = 1;
+int maxNum = 20;
+int size = 10;
+
+int [] array = CreateArray(minNum, maxNum, size);
+ShowArray(array);
+ShowArrayReverse(array);
